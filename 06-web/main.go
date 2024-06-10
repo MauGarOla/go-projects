b0109d3c7b8f58159fe946cf0eb85aaa317c49af
MauGarOla <mau.gar.ola@hotmail.com>
2024-06-10 11:18:27 +0200

    06 Basic routering and frontend

--- 06-web/main.go
+++ 06-web/main.go
@@ -1,7 +1,45 @@
-package web
+package main
 
 import (
+	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 )
 
-func home(w http.Request, r http.Response)
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	tmplPath := filepath.Join("templates", tmpl)
+	t, err := template.ParseFiles(tmplPath)
+	if err != nil {
+		http.Error(w, "Error loading template", http.StatusInternalServerError)
+		log.Println("Error parsing template:", err)
+		return
+	}
+	err = t.Execute(w, nil)
+	if err != nil {
+		http.Error(w, "Error rendering template", http.StatusInternalServerError)
+		log.Println("Error executing template:", err)
+	}
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "home.html")
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "about.html")
+}
+
+func main() {
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/about", aboutHandler)
+
+	log.Println("Server listening on port 8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Println("Error initializing the server:", err)
+	}
+}
