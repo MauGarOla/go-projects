911cb597e7d35a6a2cd894fc18a2486c90c7c86c
MauGarOla <mau.gar.ola@hotmail.com>
2024-06-15 19:59:28 +0200

    07-simple-rest finished

--- 07-simple-rest/main.go
+++ 07-simple-rest/main.go
@@ -6,14 +6,15 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
 
 type task struct {
-	ID      int    `json:ID`
-	Title   string `json:Title`
-	Content string `json:Content`
+	ID      int    `json:"ID"`
+	Title   string `json:"Title"`
+	Content string `json:"Content"`
 }
 
 type allTask []task
@@ -31,14 +32,31 @@ func indexRoute(w http.ResponseWriter, r *http.Request) {
 }
 
 func getTasks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
 	json.NewEncoder(w).Encode(exampleTasks)
+}
+
+func getTaskByID(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	taskID, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(w, "Insert a valid ID", http.StatusBadRequest)
+		return
+	}
+	for _, task := range exampleTasks {
+		if taskID == task.ID {
+			w.Header().Set("Content-type", "application/json")
+			json.NewEncoder(w).Encode(task)
+		}
+	}
 }
 
 func createTask(w http.ResponseWriter, r *http.Request) {
 	var newTask task
 	reqBody, err := io.ReadAll(r.Body)
 	if err != nil {
-		fmt.Fprintf(w, "Insert a Valid Task")
+		http.Error(w, "Insert a valid Task", http.StatusBadRequest)
+		return
 	}
 
 	json.Unmarshal(reqBody, &newTask)
@@ -51,11 +69,58 @@ func createTask(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(newTask)
 }
 
+func deleteTaskByID(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	taskID, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(w, "Insert a valid ID", http.StatusBadRequest)
+		return
+	}
+	for i, task := range exampleTasks {
+		if taskID == task.ID {
+			exampleTasks = append(exampleTasks[:i], exampleTasks[i+1:]...)
+			fmt.Fprintf(w, "The Task with the ID %v has been remove succesfully", taskID)
+		}
+	}
+}
+
+func updateTaskByID(w http.ResponseWriter, r *http.Request) {
+	var updatedTask task
+	vars := mux.Vars(r)
+
+	taskID, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(w, "Insert a valid ID", http.StatusBadRequest)
+		return
+	}
+
+	reqBody, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Insert a valid Task", http.StatusBadRequest)
+		return
+	}
+
+	json.Unmarshal(reqBody, &updatedTask)
+
+	for i, task := range exampleTasks {
+		if taskID == task.ID {
+			updatedTask.ID = taskID
+			before := append(exampleTasks[:i], updatedTask)
+			exampleTasks = append(before, exampleTasks[i+1:]...)
+			fmt.Fprintf(w, "The Task with the ID %v has been updated succesfully", taskID)
+		}
+	}
+}
+
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 
 	router.HandleFunc("/", indexRoute)
-	router.HandleFunc("/tasks", getTasks)
+	router.HandleFunc("/tasks", getTasks).Methods("GET")
+	router.HandleFunc("/tasks", createTask).Methods("POST")
+	router.HandleFunc("/tasks/{id}", getTaskByID).Methods("GET")
+	router.HandleFunc("/tasks/{id}", deleteTaskByID).Methods("DELETE")
+	router.HandleFunc("/tasks/{id}", updateTaskByID).Methods("PUT")
 
 	log.Fatal(http.ListenAndServe(":3000", router))
 }
